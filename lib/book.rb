846d390e95790daf3d2d8142b36c6347467d0736
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-10-14 16:51:11 -0500

    commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book 
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title) 
+        @title = title
+    end
+
+    def turn_page=(turn_page) 
+        @turn_page = turn_page
+    end
+
+    def turn_page 
+        @turn_page = puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe 
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand) 
+        @brand = brand 
+    end
+
+    def cobble=(cobble)
+        @cobble = cobble
+    end
+
+    def cobble 
+        @cobble = puts "Your shoe is as good as new!"
+        shoe.condition = "new"
+    end
+end

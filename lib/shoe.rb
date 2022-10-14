4ddfd4c28c4c0b39b3650a29e7689bf832b515e7
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-10-14 16:54:56 -0500

    commit tree fd7441f405120d83794f3d324a4037b464f41b04

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,6 +12,20 @@ class Shoe
 
     def cobble 
         @cobble = puts "Your shoe is as good as new!"
-        shoe.condition = "new"
+        @condition = "new"
     end
 end
+
+# class Shoe
+#     attr_accessor :color, :size, :material, :condition
+#     attr_reader :brand
+  
+#     def initialize(brand)
+#       @brand = brand
+#     end
+  
+#     def cobble
+#       @condition = "new"
+#       puts "Your shoe is as good as new!"
+#     end
+#   end
